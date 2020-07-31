7f9ea895c7051ea25255688e0dd75dfb82d78dfc
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-07-31 14:07:52 -0400

    black and pep8

--- src/zarr_jpeg/zarr_jpeg.py
+++ src/zarr_jpeg/zarr_jpeg.py
@@ -3,9 +3,10 @@ from numcodecs.compat import ensure_ndarray, ensure_contiguous_ndarray, ndarray_
 from numcodecs.registry import register_codec
 from imagecodecs import jpeg_encode, jpeg_decode
 
+
 class jpeg(Codec):
     """Codec providing jpeg compression via imagecodecs.
-    
+
     Parameters
     ----------
     quality : int
@@ -13,28 +14,31 @@ class jpeg(Codec):
     """
 
     codec_id = "jpeg"
-    
+
     def __init__(self, quality=100):
         self.quality = quality
-        assert self.quality > 0 and self.quality <= 100 and isinstance(self.quality, int)
+        assert (
+            self.quality > 0 and self.quality <= 100 and isinstance(self.quality, int)
+        )
         super().__init__()
-    
+
     def encode(self, buf):
-        bufa = ensure_ndarray(buf)        
-        assert (bufa.ndim == 2 or bufa.ndim == 3)
+        bufa = ensure_ndarray(buf)
+        assert bufa.ndim == 2 or bufa.ndim == 3
         if bufa.ndim == 3:
             tiled = bufa.reshape(bufa.shape[0] * bufa.shape[1], -1)
-        else: 
+        else:
             tiled = bufa
         return jpeg_encode(tiled, level=self.quality)
-    
-    def decode(self, buf, out=None):                  
+
+    def decode(self, buf, out=None):
         buf = ensure_contiguous_ndarray(buf)
-        
-        if out is not None:           
+
+        if out is not None:
             out = ensure_contiguous_ndarray(out)
-    
-        tiled = jpeg_decode(buf)                
+
+        tiled = jpeg_decode(buf)
         return ndarray_copy(tiled, out)
+
 
 register_codec(jpeg)
--- test/test_jpeg.py
+++ test/test_jpeg.py
@@ -22,9 +22,13 @@ codecs = [
 # only 2D and 3D shapes
 # mix of orders: C, F
 arrays = [
-    np.arange(100, dtype='u1').reshape((10,10)),        
-    np.random.randint(0, 255, size=10 * 11 * 12, dtype='u1').reshape(10, 11, 12, order='F'),
-    np.random.randint(0, 255, size=10 * 11 * 12, dtype='u1').reshape(10, 11, 12, order='C'),
+    np.arange(100, dtype="u1").reshape((10, 10)),
+    np.random.randint(0, 255, size=10 * 11 * 12, dtype="u1").reshape(
+        10, 11, 12, order="F"
+    ),
+    np.random.randint(0, 255, size=10 * 11 * 12, dtype="u1").reshape(
+        10, 11, 12, order="C"
+    ),
 ]
 
 
@@ -38,26 +42,29 @@ def test_eq():
     assert not jpeg() != jpeg()
     assert jpeg(1) == jpeg(1)
     assert jpeg(1) != jpeg(90)
-    assert jpeg() != 'foo'
-    assert 'foo' != jpeg()
-    assert not jpeg() == 'foo'
+    assert jpeg() != "foo"
+    assert "foo" != jpeg()
+    assert not jpeg() == "foo"
 
 
 def test_err_encode_list():
-    data = ['foo', 'bar', 'baz']
+    data = ["foo", "bar", "baz"]
     for codec in codecs:
         with pytest.raises(TypeError):
             codec.encode(data)
 
 
 def test_err_out_too_small():
-    arr = np.arange(120, dtype='u1').reshape(15,8)
+    arr = np.arange(120, dtype="u1").reshape(15, 8)
     out = np.empty_like(arr)[:-1]
     for codec in codecs:
         with pytest.raises(ValueError):
             codec.decode(codec.encode(arr), out)
 
+
 def test_planarization():
     for array, codec in product(arrays, codecs):
-        if array.ndim > 2: 
-            assert codec.encode(array) == codec.encode(array.reshape(array.shape[0] * array.shape[1], -1))
+        if array.ndim > 2:
+            assert codec.encode(array) == codec.encode(
+                array.reshape(array.shape[0] * array.shape[1], -1)
+            )
